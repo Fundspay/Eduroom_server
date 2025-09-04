14167dff0dc485c1ef1207648f620524e5f3a1ba
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-04 17:58:19 +0530

    feat(tutor.model):added tutor model

---
+++ models/tutor.model.js
@@ -0,0 +1,26 @@
+"use strict";
+module.exports = (sequelize, Sequelize) => {
+  const Tutor = sequelize.define(
+    "Tutor",
+    {
+      id: { type: Sequelize.BIGINT, primaryKey: true, autoIncrement: true },
+      userId: { type: Sequelize.BIGINT, allowNull: false },
+      name: { type: Sequelize.STRING, allowNull: false },
+      description: { type: Sequelize.TEXT, allowNull: true },
+      courseId: { type: Sequelize.BIGINT, allowNull: true }
+      
+    },
+    {
+      tableName: "Tutors",
+      timestamps: true
+    }
+  );
+
+  Tutor.associate = function (models) {
+    Tutor.belongsTo(models.User, { foreignKey: "userId", onDelete: "CASCADE" });
+    Tutor.hasMany(models.Course, { foreignKey: "tutorId", onDelete: "CASCADE" });
+    Tutor.belongsTo(models.Course, { foreignKey: "courseId", onDelete: "CASCADE" });
+  };
+
+  return Tutor;
+};
