b7059b2e8e7673d7d0c769e707d6523abfa8dc0a
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-04 17:57:16 +0530

    feat(casestudy.model):added casestudy model

---
+++ models/casestudy.model.js
@@ -0,0 +1,25 @@
+"use strict";
+module.exports = (sequelize, Sequelize) => {
+  const CaseStudy = sequelize.define(
+    "CaseStudy",
+    {
+      id: { type: Sequelize.BIGINT, primaryKey: true, autoIncrement: true },
+      userId: { type: Sequelize.BIGINT, allowNull: false },
+      courseId: { type: Sequelize.BIGINT, allowNull: false }, // ✅ direct link to course
+      problemStatement: { type: Sequelize.TEXT, allowNull: false },
+      answerKeywords: { type: Sequelize.TEXT, allowNull: true },
+      result: { type: Sequelize.TEXT, allowNull: true }
+    },
+    {
+      tableName: "CaseStudies",
+      timestamps: true
+    }
+  );
+
+  CaseStudy.associate = function (models) {
+    CaseStudy.belongsTo(models.User, { foreignKey: "userId", onDelete: "CASCADE" });
+    CaseStudy.belongsTo(models.Course, { foreignKey: "courseId", onDelete: "CASCADE" });
+  };
+
+  return CaseStudy;
+};
