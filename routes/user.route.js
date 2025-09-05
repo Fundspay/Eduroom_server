7b67dad5c49d956a7fd9646adc534219a4e09656
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-05 15:15:42 +0530

    updated user route '

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,12 +3,21 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 const { firebaseAuth } = require('../middleware/auth.middleware');
+const { uploadProfilePicture} = require("../middleware/s3.middleware");
 
 
 router.post("/personal-info", userController.addPersonalInfo);
 router.put("/:userId/educational-details", userController.addEducationalDetails);
 router.put("/:userId/internship-details", userController.addInternshipDetails);
-router.put("/:userId/verification-docs", userController.addVerificationDocs);
+router.post(
+  "/:userId/verification-docs",
+  uploadProfilePicture.fields([
+    { name: "studentIdCard", maxCount: 1 },
+    { name: "governmentIdProof", maxCount: 1 },
+    { name: "passportPhoto", maxCount: 1 },
+  ]),
+  userController.addVerificationDocs
+);
 router.put("/:userId/bank-details", userController.addBankDetails);
 router.put("/:userId/communication", userController.addCommunicationPreferences);
 router.put("/:userId/consent", userController.addConsent);
