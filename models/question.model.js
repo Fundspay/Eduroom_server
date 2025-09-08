d226c6111f15aaf98bceac1b47aebec574c2fb25
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-08 15:11:15 +0530

    updated question model

--- models/question.model.js
+++ models/question.model.js
@@ -7,13 +7,14 @@ module.exports = (sequelize, Sequelize) => {
             domainId: { type: Sequelize.BIGINT, allowNull: false },
             courseId: { type: Sequelize.BIGINT, allowNull: false },
             coursePreviewId: { type: Sequelize.BIGINT, allowNull: false },
-            day: { type: Sequelize.INTEGER, allowNull: false }, // Day number
+            courseDetailId: { type: Sequelize.BIGINT, allowNull: false }, // FK to CourseDetail
+            day: { type: Sequelize.INTEGER, allowNull: false }, // Day number for reference
             question: { type: Sequelize.TEXT, allowNull: false },
             optionA: { type: Sequelize.TEXT, allowNull: false },
             optionB: { type: Sequelize.TEXT, allowNull: false },
             optionC: { type: Sequelize.TEXT, allowNull: false },
             optionD: { type: Sequelize.TEXT, allowNull: false },
-            answer: { type: Sequelize.STRING, allowNull: false }, // "A", "B", "C", or "D"
+            answer: { type: Sequelize.STRING, allowNull: false }, // "A", "B", "C", "D"
             keywords: { type: Sequelize.TEXT, allowNull: true },
             caseStudy: { type: Sequelize.TEXT, allowNull: true },
             isDeleted: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
@@ -24,10 +25,10 @@ module.exports = (sequelize, Sequelize) => {
     );
 
     QuestionModel.associate = function(models) {
+        QuestionModel.belongsTo(models.CourseDetail, { foreignKey: "courseDetailId", onDelete: "CASCADE", onUpdate: "CASCADE" });
         QuestionModel.belongsTo(models.Course, { foreignKey: "courseId", onDelete: "RESTRICT", onUpdate: "RESTRICT" });
         QuestionModel.belongsTo(models.Domain, { foreignKey: "domainId", onDelete: "RESTRICT", onUpdate: "RESTRICT" });
         QuestionModel.belongsTo(models.CoursePreview, { foreignKey: "coursePreviewId", onDelete: "RESTRICT", onUpdate: "RESTRICT" });
-        QuestionModel.belongsTo(models.CourseDetail, { foreignKey: "day", targetKey: "day" }); // Map to day in CourseDetail
     };
 
     return QuestionModel;
