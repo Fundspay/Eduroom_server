63929d8a92c8c959a917efd1a9a36ab9278c4325
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-08 15:02:38 +0530

    added course revieww route

---
+++ routes/coursepreview.route.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const router = express.Router();
+const coursepreviewController = require("../controllers/coursepreviewController");
+
+// ✅ Add a new CoursePreview
+router.post("/course-preview", coursepreviewController.addCoursePreview);
+router.get("/course-previews", coursepreviewController.fetchAllCoursePreviews);
+router.get("/course-preview/:id", coursepreviewController.fetchSingleCoursePreview);
+router.put("/course-preview/:id", coursepreviewController.updateCoursePreview);
+router.delete("/course-preview/:id", coursepreviewController.deleteCoursePreview);
+router.get("/previews/:courseId", coursepreviewController.fetchPreviewsByCourse);
+
+module.exports = router;
