2443927b69b64db773fc01f2f9cd7629dc117c89
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-09 11:47:23 +0530

    CMS endpoint #wip 13

--- models/coursepreview.model.js
+++ models/coursepreview.model.js
@@ -6,15 +6,23 @@ module.exports = (sequelize, Sequelize) => {
             id: { autoIncrement: true, primaryKey: true, type: Sequelize.BIGINT },
             courseId: { type: Sequelize.BIGINT, allowNull: false },
             domainId: { type: Sequelize.BIGINT, allowNull: false },
-            
+
             title: { type: Sequelize.TEXT, allowNull: false },
             heading: { type: Sequelize.TEXT, allowNull: false },
             youtubeLink: { type: Sequelize.TEXT, allowNull: true },
             description: { type: Sequelize.TEXT, allowNull: true },
-            totalLectures: { type: Sequelize.INTEGER, allowNull: true },
+            dayCount: { type: Sequelize.INTEGER, allowNull: true },
             language: { type: Sequelize.STRING, allowNull: true },
-            whatYouWillLearn: { type: Sequelize.TEXT, allowNull: true },
-            durationPerDay: { type: Sequelize.STRING, allowNull: true },
+            level: { type: Sequelize.STRING, allowNull: true },
+
+            // ✅ JSON field for paragraph + bullet points
+            whatYouWillLearn: { 
+                type: Sequelize.JSON, 
+                allowNull: true,
+                defaultValue: { paragraph: "", bullets: [] } 
+            },
+
+            duration: { type: Sequelize.STRING, allowNull: true },
 
             isDeleted: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
             createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
