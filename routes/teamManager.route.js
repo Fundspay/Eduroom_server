b8c3ceb124cac9703a6b8aea04cde0f87706c1b7
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-10 15:03:21 +0530

    CMS endpoint #wip 64

--- routes/teamManager.route.js
+++ routes/teamManager.route.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const router = express.Router();
-const teammanagerController = require("../controllers/teamManager.controller");
+const teamManagerController = require("../controllers/teamManager.controller");
 
 // 🔹 Register Manager
 router.post("/register", teamManagerController.registerTeamManager);
 
 // 🔹 Get All Managers
-router.get("/all", teammanagerController.getAllTeamManagers);
-router.put("/update/:managerId", teammanagerController.updateTeamManager);
+router.get("/all", teamManagerController.getAllTeamManagers);
+router.put("/update/:managerId", teamManagerController.updateTeamManager);
 
 module.exports = router;
