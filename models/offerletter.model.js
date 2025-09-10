77061ee7f81e383a3cc509e85c945a623f307140
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-10 12:27:41 +0530

    CMS endpoint #wip 48

---
+++ models/offerletter.model.js
@@ -0,0 +1,33 @@
+"use strict";
+module.exports = (sequelize, Sequelize) => {
+    const OfferLetter = sequelize.define(
+        "OfferLetter",
+        {
+            id: { autoIncrement: true, primaryKey: true, type: Sequelize.BIGINT },
+            userId: { type: Sequelize.BIGINT, allowNull: false },
+            position: { type: Sequelize.STRING, allowNull: false },
+            startDate: { type: Sequelize.DATEONLY, allowNull: true },
+            location: { type: Sequelize.STRING, allowNull: true },
+            fileUrl: { type: Sequelize.STRING, allowNull: false },
+            createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
+            updatedAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW }
+
+        },
+        {
+            tableName: "OfferLetters",
+            timestamps: true,
+        }
+    );
+
+    // ✅ Associations
+    OfferLetter.associate = (models) => {
+        OfferLetter.belongsTo(models.User, {
+            foreignKey: "userId",
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+            constraints: true,
+        });
+    };
+
+    return OfferLetter;
+};
