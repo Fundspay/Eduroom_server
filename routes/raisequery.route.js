138051a822f73e13e2f9eadd6c4c09461fbf3188
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-12 14:05:00 +0530

    lms endpoint #wip for query 8

--- routes/raisequery.route.js
+++ routes/raisequery.route.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 const raisequeryController = require("../controllers/raisequery.controller");
 
-// ✅ RaiseQuery Routes
+
 router.post("/add", raisequeryController.addRaiseQuery);
 router.get("/list", raisequeryController.fetchAllRaiseQueries);
+router.get("/list/:userId", raisequeryController.fetchRaiseQueriesByUser);
 router.get("/list/:id", raisequeryController.fetchSingleRaiseQuery);
-router.put("/update/:id", raisequeryController.updateRaiseQuery);
+router.put("/update/:userId", raisequeryController.updateRaiseQueryByUser);
 
 module.exports = router;
