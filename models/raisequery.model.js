68499897758e8fa21176500f40ab34594e2c691a
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-12 14:04:50 +0530

    lms endpoint #wip for query 7

--- models/raisequery.model.js
+++ models/raisequery.model.js
@@ -4,10 +4,13 @@ module.exports = (sequelize, Sequelize) => {
     "RaiseQuery",
     {
       id: { autoIncrement: true, primaryKey: true, type: Sequelize.BIGINT },
-      userId: { type: Sequelize.BIGINT, allowNull: false },
+      userId: { type: Sequelize.BIGINT, allowNull: false }, // reference to user
       fundsAuditUserId: { type: Sequelize.BIGINT, allowNull: true }, // user assigned for funds audit
       isQueryRaised: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
       queryStatus: { type: Sequelize.STRING, allowNull: true }, // e.g., Active, Completed, Pending
+      first_name: { type: Sequelize.STRING, allowNull: true }, // store user's first name
+      last_name: { type: Sequelize.STRING, allowNull: true },  // store user's last name
+      phone_number: { type: Sequelize.STRING, allowNull: true }, // store user's phone number
       isDeleted: { type: Sequelize.BOOLEAN, allowNull: false, defaultValue: false },
       createdAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
       updatedAt: { type: Sequelize.DATE, allowNull: false, defaultValue: Sequelize.NOW },
