4c5feaaacfc99b44eb36de2b06007203bc90b14a
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-26 14:37:52 +0530

    updated fundsaudit controller

---
+++ routes/fundsaudit.route.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const router = express.Router();
+const fundsauditController = require("../controllers/fundsaudit.controller");
+
+// ✅ FundsAudit Routes
+router.post("/add", fundsauditController.addFundsAudit);               // Add a new record
+router.get("/all", fundsauditController.fetchAllFundsAudit);          // Fetch all records
+router.get("/list/:id", fundsauditController.fetchSingleFundsAudit);  // Fetch single record by ID
+router.put("/update/:id", fundsauditController.updateFundsAudit);     // Update record by ID
+router.delete("/delete/:id", fundsauditController.deleteFundsAudit);  // Delete record by ID
+
+  
+
+module.exports = router;
