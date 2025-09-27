4f13b21c1ae3b5fa0f55c70d3c8a427a191e8d0e
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-27 13:11:29 +0530

    updated raise query controller

--- routes/raisequery.route.js
+++ routes/raisequery.route.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const raisequeryController = require("../controllers/raisequery.controller");
 
 
-router.post("/add", raisequeryController.addRaiseQuery);
+router.post("/add", raisequeryController.addQuery);
 router.get("/list", raisequeryController.fetchAllRaiseQueries);
 router.get("/list/:userId", raisequeryController.fetchRaiseQueriesByUser);
 router.get("/list/:id", raisequeryController.fetchRaiseQueriesByUser);
