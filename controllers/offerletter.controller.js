b9c52e88941b44f7019f3b62b3bab7c4171a8287
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-30 12:13:20 +0530

    updated offer letter controller

--- controllers/offerletter.controller.js
+++ controllers/offerletter.controller.js
@@ -282,9 +282,177 @@ module.exports = { sendOfferLetter };
 // module.exports.listAllUsers = listAllUsers;
 
 
+// const listAllUsers = async (req, res) => {
+//   try {
+//     // Fetch users with related models
+//     const users = await User.findAll({
+//       include: [
+//         {
+//           model: TeamManager,
+//           as: "teamManager",
+//           attributes: ["id", "name", "internshipStatus"]
+//         },
+//         {
+//           model: InternshipCertificate,
+//           attributes: ["id", "courseId", "certificateUrl", "isIssued", "issuedDate"]
+//         },
+//         {
+//           model: OfferLetter,
+//           attributes: ["id", "fileUrl", "issent", "startDate"]
+//         }
+//       ]
+//     });
+
+//     const courses = await Course.findAll({
+//       attributes: ["id", "name", "duration", "businessTarget", "domainId"],
+//       include: [{ model: Domain, attributes: ["id", "name"] }]
+//     });
+
+//     const allTeamManagers = await TeamManager.findAll({
+//       where: { isDeleted: false },
+//       attributes: ["id", "managerId", "name", "email", "mobileNumber", "department", "position", "internshipStatus"]
+//     });
+
+//     const userIds = users.map(u => u.id);
+//     const raiseQueries = await RaiseQuery.findAll({
+//       where: { userId: userIds, isDeleted: false },
+//       attributes: ["userId", "isQueryRaised", "queryStatus"]
+//     });
+
+//     // Aggregate query info per user (count number of raised queries)
+//     const queryInfoByUser = {};
+//     raiseQueries.forEach(q => {
+//       if (!queryInfoByUser[q.userId]) {
+//         queryInfoByUser[q.userId] = {
+//           isQueryRaised: q.isQueryRaised || false,
+//           queryStatus: q.queryStatus || null,
+//           queryCount: q.isQueryRaised ? 1 : 0
+//         };
+//       } else {
+//         if (q.isQueryRaised) queryInfoByUser[q.userId].queryCount += 1;
+//         queryInfoByUser[q.userId].isQueryRaised = queryInfoByUser[q.userId].isQueryRaised || q.isQueryRaised;
+//         queryInfoByUser[q.userId].queryStatus = queryInfoByUser[q.userId].queryStatus || q.queryStatus;
+//       }
+//     });
+
+//     const response = [];
+
+//     for (const user of users) {
+//       // Prepare course details
+//       const courseDetails = [];
+//       if (user.courseStatuses && typeof user.courseStatuses === "object") {
+//         for (const [courseId, status] of Object.entries(user.courseStatuses)) {
+//           const course = courses.find(c => c.id.toString() === courseId);
+//           courseDetails.push({
+//             courseId,
+//             courseName: course ? course.name : null,
+//             duration: course ? course.duration : null,
+//             businessTarget: course ? course.businessTarget : null,
+//             domainName: course && course.Domain ? course.Domain.name : null,
+//             status,
+//             startDate:
+//               user.courseDates && user.courseDates[courseId]
+//                 ? user.courseDates[courseId].startDate
+//                 : null,
+//             endDate:
+//               user.courseDates && user.courseDates[courseId]
+//                 ? user.courseDates[courseId].endDate
+//                 : null
+//           });
+//         }
+//       }
+
+//       const internshipIssued =
+//         user.InternshipCertificates && user.InternshipCertificates.length > 0
+//           ? user.InternshipCertificates.some(cert => cert.isIssued)
+//           : null;
+
+//       const teamManager = user.teamManager
+//         ? {
+//           id: user.teamManager.id,
+//           name: user.teamManager.name,
+//           internshipStatus: user.teamManager.internshipStatus
+//         }
+//         : null;
+
+//       const offerLetterSent =
+//         user.OfferLetters && user.OfferLetters.length > 0
+//           ? user.OfferLetters[0].issent
+//           : false;
+
+//       const offerLetterFile =
+//         user.OfferLetters && user.OfferLetters.length > 0
+//           ? user.OfferLetters[0].fileUrl
+//           : null;
+
+//       const queryInfo = queryInfoByUser[user.id] || { isQueryRaised: false, queryStatus: null, queryCount: 0 };
+
+//       const createdAtFormatted = user.createdAt
+//         ? moment(user.createdAt).format("YYYY-MM-DD HH:mm:ss")
+//         : null;
+
+//       // Only fields that should change on update
+//       const fieldsToUpdate = {
+//         subscriptionWallet: user.subscriptionWallet,
+//         subscriptionLeft: user.subscriptionLeft,
+//         courses: courseDetails,
+//         internshipIssued,
+//         offerLetterSent,
+//         offerLetterFile,
+//         queryStatus: queryInfo.queryStatus,
+//         isQueryRaised: queryInfo.isQueryRaised,
+//         queryCount: queryInfo.queryCount,
+//         registeredAt: createdAtFormatted
+//       };
+
+//       // Check if a Status record already exists for this user
+//       let statusRecord = await Status.findOne({ where: { userId: user.id } });
+
+//       if (statusRecord) {
+//         // Update only the relevant fields
+//         await statusRecord.update(fieldsToUpdate);
+//       } else {
+//         // Create new record including teamManager & internshipStatus
+//         statusRecord = await Status.create({
+//           userId: user.id,
+//           userName: `${user.firstName} ${user.lastName}`,
+//           email: user.email,
+//           phoneNumber: user.phoneNumber,
+//           collegeName: user.collegeName,
+//           ...fieldsToUpdate,
+//           teamManager: teamManager ? teamManager.name : null,
+//           internshipStatus: teamManager ? teamManager.internshipStatus : null
+//         });
+
+//         // Save the new statusId to user
+//         await user.update({ statusId: statusRecord.id });
+//       }
+
+//       response.push({
+//         statusId: statusRecord.id,
+//         ...statusRecord.toJSON()
+//       });
+//     }
+
+//     return ReS(res, {
+//       success: true,
+//       data: response,
+//       teamManagers: {
+//         total: allTeamManagers.length,
+//         list: allTeamManagers
+//       }
+//     }, 200);
+
+//   } catch (err) {
+//     console.error("Error in listAllUsers:", err);
+//     return ReE(res, err.message, 500);
+//   }
+// };
+
+// module.exports.listAllUsers = listAllUsers; 
+
 const listAllUsers = async (req, res) => {
   try {
-    // Fetch users with related models
     const users = await User.findAll({
       include: [
         {
@@ -310,7 +478,16 @@ const listAllUsers = async (req, res) => {
 
     const allTeamManagers = await TeamManager.findAll({
       where: { isDeleted: false },
-      attributes: ["id", "managerId", "name", "email", "mobileNumber", "department", "position", "internshipStatus"]
+      attributes: [
+        "id",
+        "managerId",
+        "name",
+        "email",
+        "mobileNumber",
+        "department",
+        "position",
+        "internshipStatus"
+      ]
     });
 
     const userIds = users.map(u => u.id);
@@ -319,7 +496,6 @@ const listAllUsers = async (req, res) => {
       attributes: ["userId", "isQueryRaised", "queryStatus"]
     });
 
-    // Aggregate query info per user (count number of raised queries)
     const queryInfoByUser = {};
     raiseQueries.forEach(q => {
       if (!queryInfoByUser[q.userId]) {
@@ -330,19 +506,25 @@ const listAllUsers = async (req, res) => {
         };
       } else {
         if (q.isQueryRaised) queryInfoByUser[q.userId].queryCount += 1;
-        queryInfoByUser[q.userId].isQueryRaised = queryInfoByUser[q.userId].isQueryRaised || q.isQueryRaised;
-        queryInfoByUser[q.userId].queryStatus = queryInfoByUser[q.userId].queryStatus || q.queryStatus;
+        queryInfoByUser[q.userId].isQueryRaised =
+          queryInfoByUser[q.userId].isQueryRaised || q.isQueryRaised;
+        queryInfoByUser[q.userId].queryStatus =
+          queryInfoByUser[q.userId].queryStatus || q.queryStatus;
       }
     });
 
     const response = [];
 
+    // Counters for overall course completion
+    let totalCourses = 0;
+    let completedCourses = 0;
+
     for (const user of users) {
-      // Prepare course details
       const courseDetails = [];
       if (user.courseStatuses && typeof user.courseStatuses === "object") {
         for (const [courseId, status] of Object.entries(user.courseStatuses)) {
           const course = courses.find(c => c.id.toString() === courseId);
+
           courseDetails.push({
             courseId,
             courseName: course ? course.name : null,
@@ -359,6 +541,10 @@ const listAllUsers = async (req, res) => {
                 ? user.courseDates[courseId].endDate
                 : null
           });
+
+          // Update overall course counters
+          totalCourses++;
+          if (status === "completed") completedCourses++;
         }
       }
 
@@ -369,10 +555,10 @@ const listAllUsers = async (req, res) => {
 
       const teamManager = user.teamManager
         ? {
-          id: user.teamManager.id,
-          name: user.teamManager.name,
-          internshipStatus: user.teamManager.internshipStatus
-        }
+            id: user.teamManager.id,
+            name: user.teamManager.name,
+            internshipStatus: user.teamManager.internshipStatus
+          }
         : null;
 
       const offerLetterSent =
@@ -385,13 +571,17 @@ const listAllUsers = async (req, res) => {
           ? user.OfferLetters[0].fileUrl
           : null;
 
-      const queryInfo = queryInfoByUser[user.id] || { isQueryRaised: false, queryStatus: null, queryCount: 0 };
+      const queryInfo =
+        queryInfoByUser[user.id] || {
+          isQueryRaised: false,
+          queryStatus: null,
+          queryCount: 0
+        };
 
       const createdAtFormatted = user.createdAt
         ? moment(user.createdAt).format("YYYY-MM-DD HH:mm:ss")
         : null;
 
-      // Only fields that should change on update
       const fieldsToUpdate = {
         subscriptionWallet: user.subscriptionWallet,
         subscriptionLeft: user.subscriptionLeft,
@@ -405,14 +595,11 @@ const listAllUsers = async (req, res) => {
         registeredAt: createdAtFormatted
       };
 
-      // Check if a Status record already exists for this user
       let statusRecord = await Status.findOne({ where: { userId: user.id } });
 
       if (statusRecord) {
-        // Update only the relevant fields
         await statusRecord.update(fieldsToUpdate);
       } else {
-        // Create new record including teamManager & internshipStatus
         statusRecord = await Status.create({
           userId: user.id,
           userName: `${user.firstName} ${user.lastName}`,
@@ -424,7 +611,6 @@ const listAllUsers = async (req, res) => {
           internshipStatus: teamManager ? teamManager.internshipStatus : null
         });
 
-        // Save the new statusId to user
         await user.update({ statusId: statusRecord.id });
       }
 
@@ -434,19 +620,29 @@ const listAllUsers = async (req, res) => {
       });
     }
 
-    return ReS(res, {
-      success: true,
-      data: response,
-      teamManagers: {
-        total: allTeamManagers.length,
-        list: allTeamManagers
-      }
-    }, 200);
+    // Overall course completion percentage
+    const courseCompletionPercent =
+      totalCourses > 0 ? ((completedCourses / totalCourses) * 100).toFixed(2) : "0.00";
 
+    return ReS(
+      res,
+      {
+        success: true,
+        data: response,
+        teamManagers: {
+          total: allTeamManagers.length,
+          list: allTeamManagers
+        },
+        overallStatus: {
+          courseCompletionPercent
+        }
+      },
+      200
+    );
   } catch (err) {
     console.error("Error in listAllUsers:", err);
     return ReE(res, err.message, 500);
   }
 };
 
-module.exports.listAllUsers = listAllUsers; 
+module.exports.listAllUsers = listAllUsers;
