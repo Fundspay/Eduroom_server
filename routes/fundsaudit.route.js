4ab15a617d79a8af85cf01038d8289f1e8e341ee
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-09-30 11:00:12 +0530

    updated fundsaudit route

--- routes/fundsaudit.route.js
+++ routes/fundsaudit.route.js
@@ -8,7 +8,8 @@ router.get("/all", fundsauditController.fetchAllFundsAudit);          // Fetch a
 router.get("/list/:id", fundsauditController.fetchSingleFundsAudit);  // Fetch single record by ID
 router.put("/update/:id", fundsauditController.updateFundsAudit);     // Update record by ID
 router.delete("/delete/:id", fundsauditController.deleteFundsAudit);  // Delete record by ID
-router.get("/list", fundsauditController.listAllFundsAudit);          // List all records
+router.get("/list", fundsauditController.listAllFundsAudit);          // List all records with pagination
+router.get ("/byname", fundsauditController.listAllFundsAuditByUser); // List all records by user ID
 
   
 
