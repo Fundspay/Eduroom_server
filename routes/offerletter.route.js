2931de4bbe2a7061d7f9640e9d460943b4c56b3a
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-10-04 11:03:50 +0530

    updated offerletter route

--- routes/offerletter.route.js
+++ routes/offerletter.route.js
@@ -6,6 +6,7 @@ const offerletterController = require("../controllers/offerletter.controller");
 
 // ✅ Offer Letter Routes
 router.post("/send/:userId/:courseId", offerletterController.sendOfferLetter);
+router.post("/send/:userId", offerletterController.sendInternshipReport);
 router.get("/users/all", offerletterController.listAllUsers);
 
 module.exports = router;
