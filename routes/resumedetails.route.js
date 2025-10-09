8b23b8f3cc69313ba9484de277a8a0296db300d0
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-10-09 16:12:43 +0530

    updated resume detaisl

--- routes/resumedetails.route.js
+++ routes/resumedetails.route.js
@@ -6,16 +6,16 @@ const resumedetailsController = require("../controllers/resumedetails.controller
 
 
 router.put("/update/:id", resumedetailsController.updateResumeFields);
-router.get("/analysis/:userId", resumedetailsController.getResumeAnalysis);
-router.get("/total-analysis/:userId", resumedetailsController.gettotalResumeAnalysis);
-router.get("/analysis-per-cosheet/:userId", resumedetailsController.getResumeAnalysisPerCoSheet);
+router.get("/analysis/:teamManagerId", resumedetailsController.getResumeAnalysis);
+router.get("/total-analysis/:teamManagerId", resumedetailsController.gettotalResumeAnalysis);
+router.get("/analysis-per-cosheet/:teamManagerId", resumedetailsController.getResumeAnalysisPerCoSheet);
 router.get("/followup-totals", resumedetailsController.getFollowUpResumeTotals);
-router.get("/followup-data/:userId", resumedetailsController.getFollowUpData);
-router.get("/followups/resumes-received/:userId", resumedetailsController.getResumesReceived);
-router.get("/followups/sending-in-1-2-days/:userId", resumedetailsController.getSendingIn12Days);
-router.get("/followups/delayed/:userId", resumedetailsController.getDelayed);
-router.get("/followups/no-response/:userId", resumedetailsController.getNoResponse);
-router.get("/followups/unprofessional/:userId", resumedetailsController.getUnprofessional);
+router.get("/followup-data/:teamManagerId", resumedetailsController.getFollowUpData);
+router.get("/followups/resumes-received/:teamManagerId", resumedetailsController.getResumesReceived);
+router.get("/followups/sending-in-1-2-days/:teamManagerId", resumedetailsController.getSendingIn12Days);
+router.get("/followups/delayed/:teamManagerId", resumedetailsController.getDelayed);
+router.get("/followups/no-response/:teamManagerId", resumedetailsController.getNoResponse);
+router.get("/followups/unprofessional/:teamManagerId", resumedetailsController.getUnprofessional);
 router.get("/followups/pending", resumedetailsController.getAllPendingFollowUps);
 router.post("/send-followup-email/:id", resumedetailsController.sendFollowUpEmail);
 
