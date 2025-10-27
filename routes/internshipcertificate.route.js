6d19217e84393d42df637debef233223bb774b8d
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-10-27 14:28:56 +0530

    updated internship certificate route

--- routes/internshipcertificate.route.js
+++ routes/internshipcertificate.route.js
@@ -5,5 +5,7 @@ const internshipcertificateController = require("../controllers/internshipcertif
 // Create, deduct wallet, and send certificate in one step
 router.post("/certificate/send", internshipcertificateController.createAndSendInternshipCertificate);
 router.get("/internship/merged/email/:userId/:courseId", internshipcertificateController.generateMergedInternshipReportAndEmail);
+router.get("/listAll", internshipcertificateController.fetchAllInternshipCertificates);
+
 
 module.exports = router;
