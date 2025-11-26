b4292a83e13ab4f38694039098611b3d438d80b0
Atif-pixel <atifmulla9695@gmail.com>
2025-11-26 14:42:52 +0530

    updated the logic for get call to take from the response #1

--- controllers/bdsheet.controller.js
+++ controllers/bdsheet.controller.js
@@ -420,7 +420,7 @@ const getDashboardStats = async (req, res) => {
       if (!isNaN(taskNum)) totalAccountsSheet += taskNum;
 
       // count active interns
-      if (row.activeStatus && row.activeStatus.toLowerCase() === "Active") {
+      if (row.activeStatus && row.activeStatus.toLowerCase() === "active") {
         totalActiveInterns += 1;
       }
     });
