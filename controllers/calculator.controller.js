7832b8ea0e7dd3197f22e7d2f999accc0edd5a1e
Atif-pixel <atifmulla9695@gmail.com>
2025-11-27 11:23:15 +0530

    incentive calculator #1

--- controllers/calculator.controller.js
+++ controllers/calculator.controller.js
@@ -1,7 +1,7 @@
 "use strict";
 const model = require("../models/index");
 const { ReE, ReS } = require("../utils/util.service.js");
-const { Op } = require("sequelize");
+const { Op, Sequelize } = require("sequelize");
 
 // ---------------------------
 // Incentive Calculator
