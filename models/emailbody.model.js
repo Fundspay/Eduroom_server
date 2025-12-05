e28e955f7a03accf46e7ecddae0555dffc932a9f
Atif-pixel <atifmulla9695@gmail.com>
2025-12-05 09:52:21 +0530

    created the emailbody model for cms

---
+++ models/emailbody.model.js
@@ -0,0 +1,43 @@
+"use strict";
+module.exports = (sequelize, Sequelize) => {
+  const EmailTemplate = sequelize.define(
+    "EmailTemplate",
+    {
+      id: { type: Sequelize.BIGINT, autoIncrement: true, primaryKey: true },
+
+      // unique key like "jd_email_template"
+      key: { type: Sequelize.STRING, allowNull: false, unique: true },
+
+      subject: { type: Sequelize.TEXT, allowNull: false },
+
+      body: { type: Sequelize.TEXT("long"), allowNull: false },
+
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.NOW,
+      },
+    },
+    {
+      timestamps: true,
+      indexes: [
+        {
+          unique: true,
+          fields: ["key"],
+        },
+      ],
+    }
+  );
+
+  EmailTemplate.associate = function (models) {
+    // No associations needed now, but keeping function for future expansion
+  };
+
+  return EmailTemplate;
+};
