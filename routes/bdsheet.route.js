bef6b3275059b1db323871166cecc245dc36c7d4
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-12-17 00:36:46 +0530

    updated fundsaudit route

--- routes/bdsheet.route.js
+++ routes/bdsheet.route.js
@@ -6,13 +6,13 @@ const router = express.Router();
 const bdsheetController = require("../controllers/bdsheet.controller");
 
 // UPSERT
+router.get("/date-range", bdsheetController.getBdSheetByDateRange);
 router.post("/upsert", bdsheetController.upsertBdSheet);
 router.get("/bdsheet", bdsheetController.getBdSheet);
 router.get("/category", bdsheetController.getBdSheetByCategory);
 router.get("/activestatus", bdsheetController.getDashboardStats);
 router.post("/amount", bdsheetController.upsertRangeAmounts);
 router.get("/ranges", bdsheetController.getManagerRangeAmounts);
-router.get("/date-range", bdsheetController.getBdSheetByDateRange);
 router.get('/target-vs-achieved', bdsheetController.getTargetVsAchieved);
 router.get('/bd-tl-leaderboard', bdsheetController.getBdTlLeaderboard);
 router.get('/account-target-vs-achieved', bdsheetController.getAccountTargetVsAchieved);
