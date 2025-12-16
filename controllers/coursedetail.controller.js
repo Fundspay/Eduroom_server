6a74b19b92a27f00a2e6fc45dceb083faaa4c58f
Jeevan K Ningegowda <jeevankn934@gmail.com>
2025-12-16 09:36:25 +0530

    updated course detail controller

--- controllers/coursedetail.controller.js
+++ controllers/coursedetail.controller.js
@@ -1160,12 +1160,12 @@ const getDailyStatusAllCoursesPerUser = async (req, res) => {
         ? "Completed"
         : "In Progress";
 
-      if (
-        Number(overallCompletionRate) === 100 ||
-        (isBusinessTargetMet && allSessionsAboveThreshold)
-      ) {
+      if (isBusinessTargetMet && allSessionsAboveThreshold) {
         overallStatus = "Completed";
+      } else {
+        overallStatus = "In Progress";
       }
+
 
       const statusRecord = await model.Status.findOne({
         where: { userId, isDeleted: false },
