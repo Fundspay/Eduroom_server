dfd4ab4112d76553d91a6dda3eb762913bb0f419
Atif-pixel <atifmulla9695@gmail.com>
2025-12-30 12:15:39 +0530

    updated the analysis1 controller #5

--- controllers/analysis1.controller.js
+++ controllers/analysis1.controller.js
@@ -270,17 +270,10 @@ var getUserAnalysis = async function (req, res) {
 
     if (!record) return ReE(res, "Record not found", 404);
 
-    const { start_date, end_date } = record;
+    const { start_date, end_date, business_task } = record;
 
-    //  Fetch business task from User table (subscriptionLeft + subscriptiondeductedWallet)
-    const user = await model.User.findOne({
-      where: { id: userId },
-      attributes: ["subscriptionLeft", "subscriptiondeductedWallet"]
-    });
-
-    const taskValue =
-      (parseInt(user?.subscriptionLeft || 0, 10) +
-        parseInt(user?.subscriptiondeductedWallet || 0, 10)) || 0;
+    // Get business task value
+    const taskValue = business_task || 0;
 
     // Day-wise percentages for first 5 days
     const percentageDistribution = [18, 22, 25, 25, 10];
@@ -322,7 +315,7 @@ var getUserAnalysis = async function (req, res) {
         DATE_DAY: dateDay,
         WORK_STATUS: "Not Completed", // default
         COMMENT: "",
-        BUSINESS_TASK: taskValue, //  updated to use subscriptionLeft + subscriptiondeductedWallet
+        BUSINESS_TASK: null,
         DAILY_TARGET: i < 5 ? dailyTargets[i] || 0 : defaultTargets[i - 5],
         PERCENT_OF_WORK: "0.00%", // default
         CATEGORY: categoryDistribution[i] || ""
