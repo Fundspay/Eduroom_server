faf71c3d52d4e1534a831c891d99b3417c799afd
Atif-pixel <atifmulla9695@gmail.com>
2026-01-19 15:33:58 +0530

    added the scoresheet controller

---
+++ controllers/scoresheet.controller.js
@@ -0,0 +1,108 @@
+"use strict";
+const model = require("../models/index");
+const { ReE, ReS } = require("../utils/util.service.js");
+
+//  Upsert ScoreSheet (total score auto-calculated)
+var upsertScoreSheet = async (req, res) => {
+    const {
+        id,
+        session,
+        department,
+        link,
+        portfoliolink,
+        videolink,
+        comment,
+        manager,
+        score1,
+        score2,
+        score3,
+    } = req.body;
+
+    try {
+        // 🔹 Auto calculate total score
+        const totalScore =
+            (score1 ?? 0) +
+            (score2 ?? 0) +
+            (score3 ?? 0);
+
+        let scoreSheet;
+
+        if (id) {
+            // Update existing record
+            scoreSheet = await model.ScoreSheet.findOne({
+                where: { id },
+            });
+
+            if (!scoreSheet) {
+                return ReE(res, "ScoreSheet not found", 404);
+            }
+
+            await scoreSheet.update({
+                session: session || null,
+                department: department || null,
+                link: link || null,
+                portfoliolink: portfoliolink || null,
+                videolink: videolink || null,
+                comment: comment || null,
+                manager: manager || null,
+                score1: score1 ?? null,
+                score2: score2 ?? null,
+                score3: score3 ?? null,
+                totalscore: totalScore,
+            });
+        } else {
+            // Create new record
+            scoreSheet = await model.ScoreSheet.create({
+                session: session || null,
+                department: department || null,
+                link: link || null,
+                portfoliolink: portfoliolink || null,
+                videolink: videolink || null,
+                comment: comment || null,
+                manager: manager || null,
+                score1: score1 ?? null,
+                score2: score2 ?? null,
+                score3: score3 ?? null,
+                totalscore: totalScore,
+            });
+        }
+
+        return ReS(res, scoreSheet, 200);
+    } catch (error) {
+        return ReE(res, error.message, 422);
+    }
+};
+
+module.exports.upsertScoreSheet = upsertScoreSheet;
+
+var getScoreSheet = async (req, res) => {
+    try {
+        const { managerid } = req.params;
+
+        const whereCondition = managerid
+            ? { manager: managerid }
+            : {};
+
+        const scoreSheets = await model.ScoreSheet.findAll({
+            where: whereCondition,
+        });
+
+        const managers = await model.TeamManager.findAll({
+            attributes: ["id", "name", "email", "mobileNumber"],
+            raw: true,
+        });
+
+        return ReS(
+            res,
+            {
+                scoresheets: scoreSheets || [],
+                managers: managers || [],
+            },
+            200
+        );
+    } catch (error) {
+        return ReE(res, error.message, 422);
+    }
+};
+
+module.exports.getScoreSheet = getScoreSheet;
