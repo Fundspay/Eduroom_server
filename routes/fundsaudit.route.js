199de0159cea756c4e0dbd53c3a5fa183f66a909
Jeevan K Ningegowda <jeevankn934@gmail.com>
2026-01-28 09:51:06 +0530

    updated fundsaudit route

--- routes/fundsaudit.route.js
+++ routes/fundsaudit.route.js
@@ -14,6 +14,7 @@ router.get ("/bycollege", fundsauditController.listAllFundsAuditByCollege);
 router.get ("/count", fundsauditController.getPaidAccountsDayWise); 
 router.put("/fundsaudit/reviews/:registeredUserId", fundsauditController.upsertFundsAuditByRegisteredUser);
 router.get("/fundsaudit", fundsauditController.getAllFundsAuditList);
+router.get("/datefilterall", fundsauditController.getEntriesByDateRange);
 
 
 
