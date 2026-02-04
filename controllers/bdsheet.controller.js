e52a86d6a7bb473f31c6b7479f619fd7abeb5de3
Atif-pixel <atifmulla9695@gmail.com>
2026-02-04 16:40:08 +0530

    fixed the bdsheet controller

--- controllers/bdsheet.controller.js
+++ controllers/bdsheet.controller.js
@@ -250,7 +250,20 @@ const getBdSheet = async (req, res) => {
         {
           model: model.BdSheet,
           required: false,
-          attributes: ["id", "registration", "activeStatus", "startDate", "endDate"],
+          attributes: [
+            "id",
+            "registration",
+            "activeStatus",
+            "startDate",
+            "endDate",
+            "day1",
+            "day2",
+            "day3",
+            "day4",
+            "day5",
+            "day6",
+            "day7",
+          ],
         },
       ],
       order: [["id", "DESC"]],
@@ -344,7 +357,6 @@ const getBdSheet = async (req, res) => {
 };
 
 module.exports.getBdSheet = getBdSheet;
-
 
 const getBdSheetByCategory = async (req, res) => {
   try {
